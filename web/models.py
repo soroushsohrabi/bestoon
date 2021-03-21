ce95c2b45f0d2f3be5ce3b45aa04c9838e9cb10b
unknown <soroush.sohrabi50@gmail.com>
2021-03-21 19:45:52 +0330

    'submit/expense/

--- web/models.py
+++ web/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+class Token(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length=48)
+    def __str__(self):
+        return "{}-token".format(self.user)
+
 class Expense(models.Model):
     text = models.CharField(max_length=250)
     date = models.DateTimeField()
